75d05672520e64b32fa611feb90f39fa3d756275
SCARRAIDER <rsahanediriweera@gmail.com>
2023-07-26 13:04:26 +0530

    made changes to add new files

--- Device Finger Printing/datasets/sample/NEW.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd 
-
-df = pd.read_pickle('featureData.pkl')
-df1 = pd.read_pickle('featureData1.pkl')
-df.columns.to_frame().T.to_csv('headers.csv', index=False, header=False)
-print(df)
-print("**********")
-print(df1)
-print(df.equals(df1))
