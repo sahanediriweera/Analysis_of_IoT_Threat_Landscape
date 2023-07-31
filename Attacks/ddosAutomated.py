4e0abec6d1440f43935d56b4f4fea7a28bfddbf2
SCARRAIDER <rsahanediriweera@gmail.com>
2023-07-31 19:51:49 +0530

    Update ddosAutomated.py

--- Attacks/ddosAutomated.py
+++ Attacks/ddosAutomated.py
@@ -1,12 +1,14 @@
 import os
 import sys
+import subprocess
 
 def main(ip,fake_ip):
     directory_path = r"./"
     script_path = os.path.join(directory_path, "ddos.py")
+    command = f"python {script_path} {ip} {fake_ip}"
+    subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', command])
 
-
-    os.system(f"python {script_path} {ip} {fake_ip}")
+    # os.system(f"python {script_path} {ip} {fake_ip}")
 
 
 if __name__ == "__main__":
