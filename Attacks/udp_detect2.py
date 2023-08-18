d8745cc5d16a4603fae31ae2f04b72b969bd6a77
thanuji <tkwickramadhara@gmail.com>
2023-08-18 15:47:32 +0530

    add json to udp flood detection

--- Attacks/udp_detect2.py
+++ Attacks/udp_detect2.py
@@ -1,11 +1,13 @@
 from scapy.all import sniff, IP, UDP
 from collections import deque
 import time
+import json
 
 # Parameters for flood detection
 MAX_PACKETS = 100  # Number of packets to keep in the queue
 TIME_INTERVAL = 2  # Time interval in seconds
 THRESHOLD = 50     # Maximum number of packets within the time interval
+TIMEOUT = 60       # Timeout for packet sniffing in seconds
 
 packet_queue = deque(maxlen=MAX_PACKETS)
 start_time = time.time()
@@ -21,8 +23,13 @@ def packet_handler(packet):
         if current_time - start_time >= TIME_INTERVAL:
             if len(packet_queue) >= THRESHOLD and not udp_flood_detected:
                 udp_flood_detected = True
-                print("UDP flood detected!")
-                print(f"Source IP: {packet_queue[0][IP].src}, Destination IP: {packet_queue[0][IP].dst}")
+                result = {
+                    "alert": "UDP flood detected",
+                    "source_ip": str(packet_queue[0][IP].src),
+                    "destination_ip": str(packet_queue[0][IP].dst)
+                }
+                with open("udp_flood_alert.json", "w") as json_file:
+                    json.dump(result, json_file, indent=4)
                 # You can take further actions here, like logging, notifying, etc.
                 
             # Reset parameters for the next interval
@@ -31,4 +38,7 @@ def packet_handler(packet):
             start_time = current_time
 
 # Start sniffing packets
-sniff(filter="udp", prn=packet_handler)
+try:
+    sniff(filter="udp", prn=packet_handler, timeout=TIMEOUT)
+except KeyboardInterrupt:
+    pass  # Allow the user to interrupt the sniffing process with Ctrl+C
