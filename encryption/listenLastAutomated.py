9fb49a10d36e118e595a93b01b29f4ad8e0d2869
SCARRAIDER <rsahanediriweera@gmail.com>
2023-08-28 11:21:53 +0530

    fixed minor bugs in listen last

--- encryption/listenLastAutomated.py
+++ encryption/listenLastAutomated.py
@@ -3,6 +3,6 @@ import sys
 
 directory_path = r"./"
 script_path = os.path.join(directory_path, f"listen_last.py {sys.argv[1]} {sys.argv[2]}")
+print(script_path)
 os.system(f"sudo python {script_path}")
-
 
