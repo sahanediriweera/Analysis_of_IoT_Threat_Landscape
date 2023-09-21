4b3608a437018532bc98538e94d7fa2be4a41f6e
SCARRAIDER <rsahanediriweera@gmail.com>
2023-09-21 14:05:02 +0530

    fixed the necessary changes after minor testings

--- Attacks/startAttackScans.py
+++ Attacks/startAttackScans.py
@@ -7,9 +7,9 @@ script_path2 = os.path.join(directory_path, "starthttpAutomated.py")
 script_path3 = os.path.join(directory_path, "startICMPAutomated.py")
 script_path4 = os.path.join(directory_path, "startUdpAutomated.py")
 
-while True:
-    os.system(f"sudo python3 {script_path1}")
-    os.system(f"sudo python3 {script_path2}")
-    os.system(f"sudo python3 {script_path3}")
-    os.system(f"sudo python3 {script_path4}")
-    time.sleep(60)  # Sleep for 3 minutes (180 seconds)
+
+os.system(f"sudo python3 {script_path1}")
+os.system(f"sudo python3 {script_path2}")
+os.system(f"sudo python3 {script_path3}")
+os.system(f"sudo python3 {script_path4}")
+# Sleep for 3 minutes (180 seconds)
