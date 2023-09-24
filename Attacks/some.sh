2fb252eaa81dee209a72ca9ee089724481fb8e03
sahanediriweera <rsahanediriweera@gmail.com>
2023-09-24 18:20:06 +0530

    3rd iteration

--- Attacks/some.sh
+++ Attacks/some.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
-# Open a new terminal and run your Python script
-gnome-terminal -- bash -c "python3 ./http_detect.py; exec bash"
+
+# Replace 'your_python_script.py' with the actual name of your Python script
+python_script="http_detect.py"
+
+# Check if the Python script file exists
+if [ ! -f "$python_script" ]; then
+    echo "Error: The Python script '$python_script' does not exist."
+    exit 1
+fi
+
+# Set the terminal emulator command based on your system
+# For GNOME-based systems (e.g., Ubuntu):
+terminal_cmd="gnome-terminal -- python3 $python_script"
+
+# For systems with X Window System (generic):
+# terminal_cmd="xterm -e python3 $python_script"
+
+# Execute the terminal command
+$terminal_cmd
