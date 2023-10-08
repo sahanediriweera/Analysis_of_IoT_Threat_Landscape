07209892cf35fe9074f8c7c33945c29ece2c041e
Sahan Ediriweera <rsahanediriweera@gmail.com>
2023-10-08 22:43:48 +0530

    made changes to infinite loop with thread sleep time

--- Attacks/startAttackScans.py
+++ Attacks/startAttackScans.py
@@ -7,9 +7,10 @@ script_path2 = os.path.join(directory_path, "starthttpAutomated.py")
 script_path3 = os.path.join(directory_path, "startICMPAutomated.py")
 script_path4 = os.path.join(directory_path, "startUdpAutomated.py")
 
-
-os.system(f"sudo python3 {script_path1}")
-os.system(f"sudo python3 {script_path2}")
-os.system(f"sudo python3 {script_path3}")
-os.system(f"sudo python3 {script_path4}")
+while True:
+    os.system(f"sudo python3 {script_path1}")
+    os.system(f"sudo python3 {script_path2}")
+    os.system(f"sudo python3 {script_path3}")
+    os.system(f"sudo python3 {script_path4}")
+    time.sleep(180)
 # Sleep for 3 minutes (180 seconds)
