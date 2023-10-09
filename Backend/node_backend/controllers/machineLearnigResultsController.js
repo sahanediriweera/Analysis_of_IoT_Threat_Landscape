f062796b1197808cba93e63a1598791cd8148f16
Sahan Ediriweera <rsahanediriweera@gmail.com>
2023-10-09 17:07:55 +0530

    completed routings in machine learning

---
+++ Backend/node_backend/controllers/machineLearnigResultsController.js
@@ -0,0 +1,19 @@
+const { spawn } = require('child_process');
+const path = require('path');
+const fsPromises = require('fs').promises;
+
+const handleGetRequest = async (req, res) => {
+
+    try{
+        const data = await fsPromises.readFile(path.join(__dirname,'..','..','..','Device Finger Printing','predictions.json'),'utf8');
+        console.log(data);
+        res.json(data);
+    }catch(err){
+        console.log(err);
+        res.send("Bad Luck try again");
+        res.status(400).json({message:err});
+    }
+
+};
+
+module.exports = handleGetRequest;
--- Device Finger Printing/startMachinelearningAutomated.py
+++ Device Finger Printing/startMachinelearningAutomated.py
@@ -4,8 +4,8 @@ import time
 directory_path = r"./"
 script_path1 = os.path.join(directory_path, "convertPCAP2CSVAutomated.py")
 script_path2 = os.path.join(directory_path, "convertCSV2FeatureDataAutomated.py")
-script_path3 = os.path.join(directory_path, "model_Training_automation.py")
-script_path4 = os.path.join(directory_path, "model_inference.py")
+script_path3 = os.path.join(directory_path, "model_inference.py")
+script_path4 = os.path.join(directory_path, "predictions_to_ip.py")
 
 while True:
     os.system(f"sudo python3 {script_path1}")
